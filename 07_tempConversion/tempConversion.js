b235a2af5487f11c284fcb16a1c3f2790ed17660
michaelsam <137851066+msam4@users.noreply.github.com>
2024-08-28 11:19:35 +0900

    Challenge complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  var celsius = (temp - 32) * (5/9);
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  var fahrenheit = (9/5) * temp + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
